b246041cd1fb9a12eb4d2c88a78c7357a709335d
갱스타 <47084163+sixzer0@users.noreply.github.com>
2021-09-09 14:47:47 +0900

    Add files via upload

---
+++ router/main.js
@@ -0,0 +1,16 @@
+module.exports = function(app)
+{
+    app.get('/',function(req,res){
+        res.render('index.html');
+    });
+
+    app.get('/about',function(req,res){
+        res.render('about.html');
+    });
+    app.get('/intro',function(req,res){
+        res.render('intro.html');
+    });
+	
+	
+
+}
